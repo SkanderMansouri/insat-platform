d4b4eb0e3fcf9226348e4411b909ba06a53785a8
Emna <ksontiniaa@gmail.com>
2018-12-01 09:14:20 +0100

    adding necessary tests and use responseEntity in the controller ( task: USER CAN INVITE ANOTHER USER VIA EMAIL)

--- src/main/java/insat/company/platform/service/MailService.java
+++ src/main/java/insat/company/platform/service/MailService.java
@@ -38,7 +38,8 @@ public class MailService {
 
     private final SpringTemplateEngine templateEngine;
 
-    public MailService(JHipsterProperties jHipsterProperties, JavaMailSender javaMailSender, MessageSource messageSource, SpringTemplateEngine templateEngine) {
+    public MailService(JHipsterProperties jHipsterProperties, JavaMailSender javaMailSender,
+                       MessageSource messageSource, SpringTemplateEngine templateEngine) {
 
         this.jHipsterProperties = jHipsterProperties;
         this.javaMailSender = javaMailSender;

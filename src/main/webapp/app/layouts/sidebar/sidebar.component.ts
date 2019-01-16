b608783d0d85eb0363dbcdefb48ecbc9e607d45f
Emna <ksontiniaa@gmail.com>
2019-01-16 11:30:19 +0100

    sidebar

---
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiLanguageService } from 'ng-jhipster';
+import { SessionStorageService } from 'ngx-webstorage';
+
+import { VERSION } from 'app/app.constants';
+import { JhiLanguageHelper, Principal, LoginModalService, LoginService } from 'app/core';
+import { ProfileService } from '../profiles/profile.service';
+
+@Component({
+    selector: 'jhi-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['sidebar.css']
+})
+export class SidebarComponent implements OnInit {
+    inProduction: boolean;
+    issidebarCollapsed: boolean;
+    languages: any[];
+    swaggerEnabled: boolean;
+    modalRef: NgbModalRef;
+    version: string;
+
+    constructor(
+        private loginService: LoginService,
+        private languageService: JhiLanguageService,
+        private languageHelper: JhiLanguageHelper,
+        private sessionStorage: SessionStorageService,
+        private principal: Principal,
+        private loginModalService: LoginModalService,
+        private profileService: ProfileService,
+        private router: Router
+    ) {
+        this.version = VERSION ? 'v' + VERSION : '';
+        this.issidebarCollapsed = true;
+    }
+
+    ngOnInit() {
+        this.languageHelper.getAll().then(languages => {
+            this.languages = languages;
+        });
+
+        this.profileService.getProfileInfo().then(profileInfo => {
+            this.inProduction = profileInfo.inProduction;
+            this.swaggerEnabled = profileInfo.swaggerEnabled;
+        });
+    }
+
+    changeLanguage(languageKey: string) {
+        this.sessionStorage.store('locale', languageKey);
+        this.languageService.changeLanguage(languageKey);
+    }
+
+    collapsesidebar() {
+        this.issidebarCollapsed = true;
+    }
+
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
+    }
+
+    login() {
+        this.modalRef = this.loginModalService.open();
+    }
+
+    logout() {
+        this.collapsesidebar();
+        this.loginService.logout();
+        this.router.navigate(['']);
+    }
+
+    togglesidebar() {
+        this.issidebarCollapsed = !this.issidebarCollapsed;
+    }
+
+    getImageUrl() {
+        return this.isAuthenticated() ? this.principal.getImageUrl() : null;
+    }
+}

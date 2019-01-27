8b550d0c5bf56cd3feac8a92754959634a3a07dc
Meriem_Ben_Chaaben <meriembenchaaben97@gmail.com>
2019-01-27 19:45:23 +0100

    manage the join club process no design

--- src/main/webapp/app/entities/club/club-list.component.ts
+++ src/main/webapp/app/entities/club/club-list.component.ts
@@ -1,12 +1,124 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
+import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
+
+import { IClub } from 'app/shared/model/club.model';
+import { Principal } from 'app/core';
+import { ClubService } from './club.service';
 
 @Component({
-    selector: 'jhi-club-list',
+    selector: 'jhi-club',
     templateUrl: './club-list.component.html',
-    styles: []
+    styleUrls: ['club.css']
 })
-export class ClubListComponent implements OnInit {
-    constructor() {}
+export class ClubListComponent implements OnInit, OnDestroy {
+    clubs: IClub[];
+    currentAccount: any;
+    eventSubscriber: Subscription;
+    currentSearch: string;
+    UsersClubs: IClub[];
+    NotUserClubs: IClub[];
+    usersRequest: IClub[];
+    refresh: Subject<any> = new Subject();
 
-    ngOnInit() {}
+    constructor(
+        private clubService: ClubService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private activatedRoute: ActivatedRoute,
+        private principal: Principal
+    ) {
+        this.currentSearch =
+            this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search']
+                ? this.activatedRoute.snapshot.params['search']
+                : '';
+    }
+
+    loadAll() {
+        if (this.currentSearch) {
+            this.clubService
+                .search({
+                    query: this.currentSearch
+                })
+                .subscribe((res: HttpResponse<IClub[]>) => (this.clubs = res.body), (res: HttpErrorResponse) => this.onError(res.message));
+            return;
+        }
+        this.clubService.query().subscribe(
+            (res: HttpResponse<IClub[]>) => {
+                this.clubs = res.body;
+                this.currentSearch = '';
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    search(query) {
+        if (!query) {
+            return this.clear();
+        }
+        this.currentSearch = query;
+        this.loadAll();
+    }
+
+    clear() {
+        this.currentSearch = '';
+        this.loadAll();
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInClubs();
+        this.UsersClubs = [];
+        this.clubService.clubsUserList().subscribe(clubsList => {
+            this.UsersClubs = clubsList;
+        });
+        this.NotUserClubs = [];
+        this.clubService.NotclubsUserList().subscribe(clubsList => {
+            this.NotUserClubs = clubsList;
+        });
+        this.usersRequest = [];
+        this.clubService.RequestsList().subscribe(clubsList => {
+            this.usersRequest = clubsList;
+        });
+        this.clubService.refresh.subscribe(() => {
+            this.getAllRequest();
+        });
+        this.clubService.refresh.subscribe(() => {
+            this.getAllNonClubs();
+        });
+    }
+
+    getAllNonClubs() {
+        this.clubService.NotclubsUserList().subscribe(clubsList => {
+            this.NotUserClubs = clubsList;
+        });
+    }
+    getAllRequest() {
+        this.clubService.RequestsList().subscribe(clubsList => {
+            this.usersRequest = clubsList;
+        });
+    }
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: IClub) {
+        return item.id;
+    }
+
+    registerChangeInClubs() {
+        this.eventSubscriber = this.eventManager.subscribe('clubListModification', response => this.loadAll());
+    }
+    createJoinRequest(idClub: number) {
+        this.clubService.createRequest(idClub).subscribe(request => {});
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
